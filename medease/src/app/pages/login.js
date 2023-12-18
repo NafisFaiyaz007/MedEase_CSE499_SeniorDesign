b27778fca46751fa5437ceff418b7b059ab67d0b
Nafis Faiyaz <56973454+NafisFaiyaz007@users.noreply.github.com>
2023-12-18 12:47:22 +0600

    bismillah

---
+++ medease/src/app/pages/login.js
@@ -0,0 +1,52 @@
+// pages/Login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md w-full p-6 bg-white shadow-md rounded-md">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="mt-1 p-2 w-full border rounded-md"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 p-2 w-full border rounded-md"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
